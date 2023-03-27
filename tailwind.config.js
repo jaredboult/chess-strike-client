0cf5cf9ba0297eb0c750b62e12b135b671e0025f
Jared Boult <16514321+jaredboult@users.noreply.github.com>
2023-03-27 23:10:06 +1300

    Logo + styles for light and dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,7 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {
-      backgroundColor: {
-        'light-cell': '#f0d9b5',
-        'dark-cell': '#b58863',
-      },
-      padding: {
-        '1/2': '50%',
-        full: '100%',
-      },
-    },
+    extend: {},
     plugins: [],
   }
 }
